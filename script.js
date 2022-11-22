f753733ad482abe5fcc0b71c0dc4b7610c7ce3f5
Luíza Barbosa <108764279+aziulll@users.noreply.github.com>
2022-11-22 18:23:17 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function contarNumero() {
+ 
+    var numberOne = document.getElementById("numberOne").value;
+    var numberTwo = document.getElementById("numberTwo").value;
+    var resultado = document.getElementById("resultado");
+   
+    var soma = Number(numberOne) + Number(numberTwo);
+
+if (numberOne === '' || numberTwo === '') {
+    return resultado.innerHTML = `Preencha os campos`
+}
+
+
+    resultado.innerHTML = `o resultado é ${soma}`
+    return;
+} 
+   
+
+
